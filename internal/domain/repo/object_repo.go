3136e0027fdbaca27450c540c33dbb6ab08c8166
mikuto <mikuto1227k@gmail.com>
2025-04-29 01:03:08 +0000

    fix:IobjectsRepoで定義した戻り値をレポジトリレイヤーと合うように修正

--- internal/domain/repo/object_repo.go
+++ internal/domain/repo/object_repo.go
@@ -3,6 +3,6 @@ package repo
 import "github.com/mikuto0831/cybozu-http-server/internal/domain/entity"
 
 type IObjectsRepo interface {
-	GetObjectByID(id string) (entity.Object, error)
+	GetObjectByID(id string) (*entity.Object, error)
 	PutObject(object entity.Object) error
 }
